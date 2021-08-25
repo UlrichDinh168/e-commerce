03758c43ee30c6c10f408fb1d3425b1f6213f0d7
Ulrich Dinh <duong0028@gmail.com>
2021-08-25 22:34:55 +0300

    config reducer and add necessary files

---
+++ client/src/reducers/index.js
@@ -0,0 +1,39 @@
+/**
+ * Root reducer
+ *
+ *
+ */
+
+import { combineReducers } from "redux";
+import { connectRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+import { loginTypes as types } from "actions/types";
+import storage from "redux-persist/es/storage";
+import { PERSIST_KEY } from "constants";
+// Login
+import { loginReducer, loginSelectors } from "./login";
+import { notificationReducer, notificationSelectors } from "./notification";
+
+// export root reducer
+const index = combineReducers({
+  router: connectRouter(history),
+  login: loginReducer,
+  notification: notificationReducer,
+});
+export default (state, action) => {
+  if (
+    action.type === types.logoutSuccess ||
+    action.type === types.localLogout
+  ) {
+    storage.removeItem(PERSIST_KEY);
+    state = undefined;
+  }
+  return index(state, action);
+};
+// export history
+export { history };
+
+// export getters/ selectors
+export { loginSelectors, notificationSelectors };
