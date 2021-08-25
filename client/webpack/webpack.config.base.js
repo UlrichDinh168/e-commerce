77af0416fab1fe5c4495c1caf4f562ed785340ff
Ulrich Dinh <duong0028@gmail.com>
2021-08-25 22:36:36 +0300

    config webpack and babel

---
+++ client/webpack/webpack.config.base.js
@@ -0,0 +1,99 @@
+/**
+ * Common webpack configuration
+ *
+ *
+ */
+
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const { helpers } = require("chart.js");
+const webpack = require("webpack");
+module.exports = {
+  entry: path.resolve(__dirname, "../src/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "../build"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      constants: path.resolve(__dirname, "../src/constants.js"),
+      shared: path.resolve(__dirname, "../src/shared"),
+      services: path.resolve(__dirname, "../src/services"),
+      assets: path.resolve(__dirname, "../src/assets"),
+      pages: path.resolve(__dirname, "../src/pages"),
+      helpers: path.resolve(__dirname, "../src/helpers"),
+      hooks: path.resolve(__dirname, "../src/hooks"),
+      actions: path.resolve(__dirname, "../src/actions"),
+      reducers: path.resolve(__dirname, "../src/reducers"),
+      hocs: path.resolve(__dirname, "../src/hocs"),
+      styles: path.resolve(__dirname, "../src/styles"),
+      utils: path.resolve(__dirname, "../src/utils"),
+      modules: path.resolve(__dirname, "../src/modules"),
+      data: path.resolve(__dirname, "../src/data"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|otf|ttf|woff|woff2)$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "url-loader",
+          },
+        ],
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "markdown-loader",
+            options: {
+              pedantic: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins:
+    process.env.NODE_ENV === "development"
+      ? [new Dotenv()]
+      : [
+          new webpack.DefinePlugin({
+            "process.env": {
+              BACKEND_BASE_URL: JSON.stringify(process.env.BACKEND_BASE_URL),
+            },
+          }),
+          new Dotenv({
+            systemvars: true,
+          }),
+        ],
+};
---
+++ client/webpack/webpack.config.dev.js
@@ -0,0 +1,67 @@
+/**
+ * Webpack config for building front-end in development
+ *
+ *
+ */
+
+const path = require("path");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const base = require("./webpack.config.base");
+const loadPresets = require("./loadPresets");
+
+module.exports = (env = "development") =>
+  merge(
+    base,
+    {
+      mode: "development",
+      devtool: "eval-source-map",
+      output: {
+        filename: "[name].js",
+        chunkFilename: "[name].chunk.js",
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(?:sa|sc|c)ss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
+        ],
+      },
+      devServer: {
+        // stats: {
+        //   colors: true,
+        //   hash: false,
+        //   version: false,
+        //   timings: true,
+        //   assets: false,
+        //   chunks: false,
+        //   modules: false,
+        //   reasons: false,
+        //   children: false,
+        //   source: true,
+        //   errors: true,
+        //   errorDetails: false,
+        //   warnings: false,
+        //   publicPath: false,
+        // },
+        // server default port
+        port: 3001,
+        historyApiFallback: true,
+        hot: true,
+        // contentBase: path.resolve(__dirname, "./public/"),
+        static: "./public",
+      },
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebPackPlugin({
+          inject: true,
+          template: "public/index.html",
+          filename: "index.html",
+        }),
+      ],
+    },
+    loadPresets(env)
+  );
